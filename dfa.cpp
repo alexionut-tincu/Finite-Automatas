8a81c2375aeacf76e1be82e2731ba6cd52059a72
alexionut05 <alexionut05@users.noreply.github.com>
2024-04-05 04:17:04 +0300

    Refactor DFA class (remove state aliasing) and update main function

--- dfa.cpp
+++ dfa.cpp
@@ -1,52 +1,52 @@
 #include <fstream>
-#include <vector>
+#include <set>
 #include <string>
 #include <unordered_map>
+#include <vector>
 
 struct DFA {
+	int states_count;
+	std::set<int> states;
 	std::unordered_map<int, std::unordered_map<char, int>> transition_function;
-	std::unordered_map<int, int> states;
-	std::vector<bool> final_states;
+	std::set<int> final_states;
 	int init_state;
 
-	DFA(const int states_count) : final_states(states_count, false) {};
+	DFA(const int states_count) : states_count(states_count) {};
 
-	void AddState(const int alias, const int state)
+	void AddState(const int state)
 	{
-		states[alias] = state;
+		states.insert(state);
 	}
 
-	void AddTransition(const int start_state_alias, 
+	void AddTransition(const int start_state, 
 			   const char letter,
-			   const int end_state_alias)
+			   const int end_state)
 	{
-		int start_state = states[start_state_alias];
-		int end_state = states[end_state_alias];
 		transition_function[start_state][letter] = end_state;
 	}
 
-	void SetInitState(const int state_alias)
+	void SetInitState(const int state)
 	{
-		int state = states[state_alias];
 		init_state = state;
 	}
 
-	void SetFinalState(const int state_alias)
+	void SetFinalState(const int state)
 	{
-		int state = states[state_alias];
-		final_states[state] = true;
+		final_states.insert(state);
 	}
 
 	bool AcceptWord(const std::string &word)
 	{
 		int current_state = init_state;
+
 		for (char letter : word) {
 			current_state = transition_function[current_state][letter];
 			if (current_state == 0) {
 				return false;
 			}
 		}
-		return final_states[current_state];
+		
+		return (final_states.count(current_state) > 0);
 	}
 };
 
@@ -59,40 +59,40 @@ int main()
 	int states_count;
 	fin >> states_count;
 	DFA dfa(states_count);
-	for (int i = 1; i <= states_count; ++i) {
-		int state_alias;
-		fin >> state_alias;
-		dfa.AddState(state_alias, i);
+	for (int i = 0; i < states_count; ++i) {
+		int state;
+		fin >> state;
+		dfa.AddState(state);
 	}
 
 	// Read transitions count and transitions
 	int transitions_count;
 	fin >> transitions_count;
-	for (int i = 1; i <= transitions_count; ++i) {
-		int start_state_alias, end_state_alias;
+	for (int i = 0; i < transitions_count; ++i) {
+		int start_state, end_state;
 		char letter;
-		fin >> start_state_alias >> end_state_alias >> letter;
-		dfa.AddTransition(start_state_alias, letter, end_state_alias);
+		fin >> start_state >> end_state >> letter;
+		dfa.AddTransition(start_state, letter, end_state);
 	}
 
 	// Read init state
-	int init_state_alias;
-	fin >> init_state_alias;
-	dfa.SetInitState(init_state_alias);
+	int init_state;
+	fin >> init_state;
+	dfa.SetInitState(init_state);
 
 	// Read final states count and final states
 	int final_states_count;
 	fin >> final_states_count;
-	for (int i = 1; i <= final_states_count; ++i) {
-		int final_state_alias;
-		fin >> final_state_alias;
-		dfa.SetFinalState(final_state_alias);
+	for (int i = 0; i < final_states_count; ++i) {
+		int final_state;
+		fin >> final_state;
+		dfa.SetFinalState(final_state);
 	}
 
 	// Read number of words and words
 	int words_count;
 	fin >> words_count;
-	for (int i = 1; i <= words_count; ++i) {
+	for (int i = 0; i < words_count; ++i) {
 		std::string word;
 		fin >> word;
 		fout << (dfa.AcceptWord(word) ? "DA\n" : "NU\n");
